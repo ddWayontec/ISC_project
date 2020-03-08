fa60ba9ca416c561f9b142ce29780684a64fbf24
s-seguin <stuart.seguin@ucalgary.ca>
2020-03-08 12:55:11 -0600

    feat(isc-frontend): add logic to sidebar

--- isc-frontend/src/components/side-bar/side-bar.jsx
+++ isc-frontend/src/components/side-bar/side-bar.jsx
@@ -1,76 +1,78 @@
-import React from 'react';
+import React from "react";
 
 import {
   Drawer,
-  Divider,
   List,
   ListItem,
   ListItemIcon,
   ListItemText,
-  makeStyles,
-} from '@material-ui/core';
+  makeStyles
+} from "@material-ui/core";
 
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
+    display: "flex"
   },
   iscLogo: {
-    width: '98%',
-    display: 'block',
-    marginLeft: 'auto',
-    marginRight: 'auto',
+    width: "98%",
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto"
   },
   drawer: {
     width: drawerWidth,
     flexShrink: 0,
-    backgroundColor: theme.status.danger,
+    backgroundColor: theme.status.danger
   },
   drawerPaper: {
-    width: drawerWidth,
+    width: drawerWidth
   },
   toolbar: theme.mixins.toolbar,
   content: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.default,
-    padding: theme.spacing(3),
-  },
+    padding: theme.spacing(3)
+  }
 }));
 
-export const Sidebar = () => {
+export const Sidebar = ({ history, items }) => {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <Drawer
-        anchor={'left'}
+        anchor={"left"}
         open={true}
-        variant={'permanent'}
+        variant={"permanent"}
         className={classes.drawer}
         classes={{
-          paper: classes.drawerPaper,
+          paper: classes.drawerPaper
         }}
       >
         <div>
           <img
             alt="ISC Logo"
             className={classes.iscLogo}
-            src={process.env.PUBLIC_URL + '/ISC-Logo-simple-transparent.png'}
+            src={process.env.PUBLIC_URL + "/ISC-Logo-simple-transparent.png"}
           />
         </div>
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
+          {items.map(item => (
+            <ListItem button onClick={() => history.push(item.route)}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
             </ListItem>
           ))}
+          <ListItem button onClick={() => history.push("/login")}>
+            <ListItemIcon>
+              <ExitToAppIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Logout"} />
+          </ListItem>
         </List>
       </Drawer>
     </div>
--- isc-frontend/src/routes/home/home.jsx
+++ isc-frontend/src/routes/home/home.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
 
-import { AppContainer, Sidebar } from '../../components';
+import { Login } from "../login";
 
-export const Home = () => {
+import { AppContainer, Sidebar } from "../../components";
+import PeopleIcon from "@material-ui/icons/People";
+import AddIcon from "@material-ui/icons/Add";
+
+import { Switch, Route } from "react-router-dom";
+
+const items = [
+  {
+    text: "List all immigrants",
+    icon: <PeopleIcon />,
+    route: "/list-all-immigrants"
+  },
+  { text: "Add immigrant", icon: <AddIcon />, route: "/add-immigrant" },
+  {
+    text: "Add service provider",
+    icon: <AddIcon />,
+    route: "/add-service-provider"
+  },
+  { text: "Add ISC employee", icon: <AddIcon />, route: "/add-isc-employee" }
+];
+
+export const Home = props => {
   return (
     <AppContainer>
-      <Sidebar />
+      <Sidebar history={props.history} items={items} />
+      <Switch>
+        <Route path="/list-all-immigrants" component={Login} />
+      </Switch>
     </AppContainer>
   );
 };
