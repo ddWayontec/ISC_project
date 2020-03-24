18168bc07d43eac54aafd34737a1fae3b02484e9
s-seguin <stuart.seguin@ucalgary.ca>
2020-03-23 21:11:18 -0600

    chore(isc-frontend): fix pre commit hook

--- isc-frontend/src/index.js
+++ isc-frontend/src/index.js
@@ -3,12 +3,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
+import App from "./App";
 import { Auth } from "./components/authentication";
 import * as serviceWorker from "./serviceWorker";
 import theme from "./theme";
-
-import App from "./App";
-
 
 ReactDOM.render(
   <Auth>
